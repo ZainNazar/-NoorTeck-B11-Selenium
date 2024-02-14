7e17652063f964d1af7d6f9e313ba786659b4efc
zalkh <zalkh@10.0.0.55>
2024-02-14 12:36:18 -0600

    Change xPath (ConfirmMessage)

--- src/test/java/a/homework/one/ExerciseTwo.java
+++ src/test/java/a/homework/one/ExerciseTwo.java
@@ -84,10 +84,9 @@ public class ExerciseTwo extends Hooks {
 
 			WebElement submitButton = driver.findElement(By.name("submit"));
 			submitButton.click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 // check The country Field
-			WebElement confirMessage = driver.findElement(By.cssSelector(
-					"body > div:nth-child(5) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(3) > td > p:nth-child(2) > font"));
+			WebElement confirMessage = driver.findElement(By.xpath("(//font[@size ='2'])[2]"));
 
 			String checkConfirm = confirMessage.getText();
 			if (checkConfirm.contains("Thank you for registering.")) {
